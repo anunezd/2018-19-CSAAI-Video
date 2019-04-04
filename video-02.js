bc6b87fc3a6c166a7a864f703b08b052abefa544
anunezd <alejandro_nd93@hotmail.com>
2019-04-04 09:50:16 +0200

    3 botones 3 videos

--- video-02.js
+++ video-02.js
@@ -4,19 +4,30 @@ function main()
 
   //-- Obtener el elemento de video
   video1 = document.getElementById("video1")
+  video2 = document.getElementById("video2")
+  video3 = document.getElementById("video3")
 
   //-- Establecer el tamaño del vídeo
   video1.width = 200;
   video1.height= 100;
+  video2.width = 200;
+  video2.height= 100;
+  video3.width = 200;
+  video3.height= 100;
 
   //-- Boton de Play
-  ver = document.getElementById('play')
+  ver1 = document.getElementById('play1')
+  ver2 = document.getElementById('play2')
+  ver3 = document.getElementById('play3')
 
   //-- Al apretar el boton de ver video
-  ver.onclick = () => {
+  ver1.onclick = () => {
     console.log("Ver!")
-
-    //-- Indicar la fuente del vídeo
-    video1.src ="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4"
-  }
+    video1.src = "https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4"}
+  ver2.onclick = () => {
+    console.log("Ver!")
+    video2.src = "https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4"}
+  ver3.onclick = () => {
+    console.log("Ver!")
+    video3.src = "https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4"}
 }
