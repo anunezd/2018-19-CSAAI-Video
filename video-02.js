74ade4bfc9c833f8169ec85643dd970968fe80ff
anunezd <alejandro_nd93@hotmail.com>
2019-05-08 12:01:29 +0200

    Realizador cada 2 seg

--- video-02.js
+++ video-02.js
@@ -38,6 +38,7 @@ function main()
     video3.muted = true;}
   video4.width = 800;
   video4.height= 400;
+  video4.src = "https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4"
 
   //-- Boton de Play
   ver1 = document.getElementById('play1')
@@ -61,14 +62,49 @@ function main()
     display.innerHTML = 'VIDEO C'
   }
 
-  video4.addEventListener("timeupdate",function(ev){
-      var time  = (video4.currentTime);
-      var hours = Math.floor( time / 3600 );
-      var minutes = Math.floor( (time % 3600) / 60 );
-      var seconds = Math.floor(time % 60);
+  var count = 0,
+      clk_time = [0,0,0],
+      count = 0,
+      actual = 1;
 
-      var result = hours + ":" + minutes + ":" + seconds;
-      clk.innerHTML = result;
-    },true);
+  setInterval(function(){
+    clk_time[2]++;
+    count++;
+    switch (clk_time[2]) {
+      case 60:
+        clk_time[2] = 0;
+        clk_time[1]++;
+        break;
+      default:
+        //
+    }
+    switch (count) {
+      case 2:
+        count = 0;
+        switch (actual) {
+          case 1:
+            actual = 2;
+            video4.src = video2.src
+            video4.currentTime = video2.currentTime
+            display.innerHTML = 'VIDEO B'
+            break;
+          case 2:
+            actual = 3;
+            video4.src = video3.src
+            video4.currentTime = video3.currentTime
+            display.innerHTML = 'VIDEO C'
+            break
+          default:
+            actual = 1;
+            video4.src = video1.src
+            video4.currentTime = video1.currentTime
+            display.innerHTML = 'VIDEO A'
+        }
+        break;
+      default:
+        //
+    }
+    clk.innerHTML = clk_time[0] + ":" + clk_time[1] + ":" + clk_time[2];
+  },1000);
 
 }
